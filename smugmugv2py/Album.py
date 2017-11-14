319fb7469396b32acbeaa0333f7374b1f662d15a
Markus Leuthold <git@titlis.org>
2017-11-14 20:32:07 +0100

    PEP8-ify
    according to PEP8 (which are more restrictive in Python 3)
    - spaces instead of tabs
    - two blank lines before class
    
    This avoids warnings in PyCharm

--- smugmugv2py/Album.py
+++ smugmugv2py/Album.py
@@ -1,35 +1,36 @@
 from .AlbumImage import AlbumImage
 
+
 class Album(object):
-	def __init__(self, album):
-		self.uri = album["Uri"]
-		self.name = album["Name"]
-		self.url_name = album["UrlName"]
-		self.description = album["Description"]
-		self.image_count = album["ImageCount"]
-		self.keywords = album["Keywords"]
-		self.sort_method = album["SortMethod"]
-		self.sort_direction = album["SortDirection"]
-		if self.image_count:
-			self.__images = album["Uris"]["AlbumImages"]
+    def __init__(self, album):
+        self.uri = album["Uri"]
+        self.name = album["Name"]
+        self.url_name = album["UrlName"]
+        self.description = album["Description"]
+        self.image_count = album["ImageCount"]
+        self.keywords = album["Keywords"]
+        self.sort_method = album["SortMethod"]
+        self.sort_direction = album["SortDirection"]
+        if self.image_count:
+            self.__images = album["Uris"]["AlbumImages"]
 
-	@classmethod
-	def get_album(cls, connection, album_uri):
-		return cls(connection.get(album_uri)["Album"])
+    @classmethod
+    def get_album(cls, connection, album_uri):
+        return cls(connection.get(album_uri)["Album"])
 
-	def get_images(self, connection):
-		ret=[]
+    def get_images(self, connection):
+        ret = []
 
-		if self.image_count:
-			images = connection.get(self.__images)["AlbumImage"]
-			for image in images:
-				thisimage = AlbumImage(image)
-				ret.append(thisimage)
+        if self.image_count:
+            images = connection.get(self.__images)["AlbumImage"]
+            for image in images:
+                this_image = AlbumImage(image)
+                ret.append(this_image)
 
-		return ret
+        return ret
 
-	def delete_album(self, connection):
-		return connection.delete(self.uri)
+    def delete_album(self, connection):
+        return connection.delete(self.uri)
 
-	def change_album(self, connection, changes):
-		return connection.patch(self.uri, changes)["Response"]["Album"]
+    def change_album(self, connection, changes):
+        return connection.patch(self.uri, changes)["Response"]["Album"]
--- test.py
+++ test.py
@@ -9,100 +9,104 @@ from datetime import datetime
 from json import dumps
 from requests import exceptions
 
+
 def do_indent(indent):
-	for x in range(0, indent):
-		stdout.write(" ")
+    for x in range(0, indent):
+        stdout.write(" ")
+
 
 def print_album(node, indent):
-	album = Album.get_album(connection, node.album_uri)
-	stdout.write(", " + str(album.image_count) + " images")
-	images = album.get_images(connection)
-	for image in images:
-		do_indent(indent+1)
-		print(image.filename + " - " + image.caption)
+    album = Album.get_album(connection, node.album_uri)
+    stdout.write(", " + str(album.image_count) + " images")
+    images = album.get_images(connection)
+    for image in images:
+        do_indent(indent + 1)
+        print(image.filename + " - " + image.caption)
+
 
 def print_node(node, indent):
-	do_indent(indent)
-	stdout.write("'" + node.name + "' (" + node.type + ") - " + node.privacy)
-	if node.type == "Album":
-		print_album(node, indent)
-	print()
-	children=node.get_children(connection)
-	for child in children:
-		print_node(child, indent+1)
+    do_indent(indent)
+    stdout.write("'" + node.name + "' (" + node.type + ") - " + node.privacy)
+    if node.type == "Album":
+        print_album(node, indent)
+    print()
+    children = node.get_children(connection)
+    for child in children:
+        print_node(child, indent + 1)
+
 
 if not api_key or not api_secret:
-  raise Exception('API key and secret are required. see test_setup.py')
+    raise Exception('API key and secret are required. see test_setup.py')
 
 connection = Connection(api_key, api_secret, user_agent="Test user agent/2.4")
 
 if not token or not secret:
-	auth_url = connection.get_auth_url(access="Full", permissions="Modify")
+    auth_url = connection.get_auth_url(access="Full", permissions="Modify")
 
-	print("Visit the following URL and retrieve a verification code:%s%s" % (linesep, auth_url))
+    print("Visit the following URL and retrieve a verification code:%s%s" % (linesep, auth_url))
 
-	stdout.write('Enter the six-digit code: ')
-	stdout.flush()
-	verifier = stdin.readline().strip()
+    stdout.write('Enter the six-digit code: ')
+    stdout.flush()
+    verifier = stdin.readline().strip()
 
-	at, ats = connection.get_access_token(verifier)
+    at, ats = connection.get_access_token(verifier)
 
-	print("Token: " + at)
-	print("Secret: " + ats)
+    print("Token: " + at)
+    print("Secret: " + ats)
 
-	token = at
-	secret = ats
+    token = at
+    secret = ats
 
 connection.authorise_connection(token, secret)
 
 try:
-	user = User.get_authorized_user(connection)
-	print("User: " + user.nickname + " (" + user.name + ")")
+    user = User.get_authorized_user(connection)
+    print("User: " + user.nickname + " (" + user.name + ")")
 
-	node = Node.get_node(connection,user.node)
-	#print_node(node, 0)
+    node = Node.get_node(connection, user.node)
+    # print_node(node, 0)
 
-	#new_node=Node(node.create_child_folder(connection, 'aaaatestnode2','aaaaatestnode2','Public'))
-	#print new_node.uri + " - " + new_node.name
+    # new_node=Node(node.create_child_folder(connection, 'aaaatestnode2','aaaaatestnode2','Public'))
+    # print new_node.uri + " - " + new_node.name
 
-	new_node = None
+    new_node = None
 
-	for thisnode in node.get_children(connection):
-		if thisnode.url_name == "Testalbum":
-			new_node=thisnode
+    for thisnode in node.get_children(connection):
+        if thisnode.url_name == "Testalbum":
+            new_node = thisnode
 
-	if new_node is None:
-		# creating the child folder privately so people can run this test script 'safely'.
-		new_node=node.create_child_album(connection, 'testalbum','Testalbum','Private', 'A long description for the album')
+    if new_node is None:
+        # creating the child folder privately so people can run this test script 'safely'.
+        new_node = node.create_child_album(connection, 'testalbum', 'Testalbum', 'Private',
+                                           'A long description for the album')
 
-	print(new_node.uri + " - " + new_node.name + new_node.album_uri)
-	album=Album.get_album(connection, new_node.album_uri)
+    print(new_node.uri + " - " + new_node.name + new_node.album_uri)
+    album = Album.get_album(connection, new_node.album_uri)
 
-	try:
-		pprint(connection.upload_image('adhawkins_github_avatar.jpg',
-											album.uri))
-	except exceptions.ConnectionError as e:
-		print("ConnectionError: " + str(e))
+    try:
+        pprint(connection.upload_image('adhawkins_github_avatar.jpg',
+                                       album.uri))
+    except exceptions.ConnectionError as e:
+        print("ConnectionError: " + str(e))
 
-		print("Deleting node after failed upload")
-		new_node.delete_node(connection)
+        print("Deleting node after failed upload")
+        new_node.delete_node(connection)
 
-	#delete_node=Node(node.create_child_folder(connection, 'deletetest','Deletetest','Public'))
-	#print "Name: " + delete_node.name + ", url: " + delete_node.url_name
-	#pprint(delete_node.delete_node(connection))
+    # delete_node=Node(node.create_child_folder(connection, 'deletetest','Deletetest','Public'))
+    # print "Name: " + delete_node.name + ", url: " + delete_node.url_name
+    # pprint(delete_node.delete_node(connection))
 
-	#rename_node=Node(node.create_child_folder(connection, 'renametest','Renametest','Public'))
-	#print "Name: " + rename_node.name + ", url: " + rename_node.url_name
+    # rename_node=Node(node.create_child_folder(connection, 'renametest','Renametest','Public'))
+    # print "Name: " + rename_node.name + ", url: " + rename_node.url_name
 
-	#rename = {
-	#	"UrlName": "Renametestchange"
-	#}
+    # rename = {
+    #	"UrlName": "Renametestchange"
+    # }
 
-	#renamed_node=Node(rename_node.change_node(connection, rename))
-	#print "Renamed Name: " + renamed_node.name + ", url: " + renamed_node.url_name
-	#found_renamed_node=Node(SmugMugv2Utils.get_node(connection, rename_node.uri))
-	#print "Found Renamed Name: " + found_renamed_node.name + ", url: " + found_renamed_node.url_name
-	#pprint(connection.delete(renamed_node.uri))
+    # renamed_node=Node(rename_node.change_node(connection, rename))
+    # print "Renamed Name: " + renamed_node.name + ", url: " + renamed_node.url_name
+    # found_renamed_node=Node(SmugMugv2Utils.get_node(connection, rename_node.uri))
+    # print "Found Renamed Name: " + found_renamed_node.name + ", url: " + found_renamed_node.url_name
+    # pprint(connection.delete(renamed_node.uri))
 except SmugMugv2Exception as e:
-	print("Error: " + str(e))
-
+    print("Error: " + str(e))
